cdc893085dd81a9f038fa9a2ecffd277b5c9185c
Changmin Lim <chang.lim92@gmail.com>
2018-06-06 13:32:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(dogName)
+    @name = dogName
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(breeder)
+    @breed = breeder 
+  end 
+end 
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(myname)
+    @name = myname 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def job=(myJob)
+    @job = myJob
+  end 
+  
+  def job 
+    @job 
+  end 
+end 
